76ee76dd1acb2c33defe3c45dd53d73b11c8bd9f
GvNiekerk <gvniekerk011@gmail.com>
2021-01-26 20:56:28 +0200

    worked on account

--- Routes/user.js
+++ Routes/user.js
@@ -1,11 +1,29 @@
 const router = require('express').Router();
 const tokenVerification = require('./verifyToken');
+const User = require('../Models/User');
 
 router.get('/getdata', tokenVerification, async (req, res) => {
 
     console.log(req.user);
-    //const userId = req.user._Id;
+    const userId = req.user._id;
+    console.log(userId);
 
+    var companyName = "fok oll";
+    var userEmail = "niks";
+    var cellNum =  "kont oll";
+    var website = "warie";
+
+    User.findOne({ _id: userId }).then(user => {
+      if (user) {
+        
+        companyName = user.companyName
+        userEmail = user.userEmail
+        cellNum = user.cellNum
+        website = user.website
+      } else {
+        return res.status(400).json({ userId: "User not found" });  
+        }
+    });
     //Logic
     //Database callings to get valid data for userId
 
@@ -16,13 +34,13 @@ router.get('/getdata', tokenVerification, async (req, res) => {
     var response = {
         totalClicks: 123,
         monthlyClicks: 20,
-        name: "John Doe",
+        name: companyName,
         location: "Centurion",
         contactInformation: {
           work: "Plumbing",
-          website: "https://pornhub.com",
-          mail: "johnnysins@pornhub.com",
-          phone: "071 6969 420"
+          website: website,
+          mail: userEmail,
+          phone: cellNum
         }
       }
 
