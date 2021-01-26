aea0306737cd153c9301739021491cda90bd95d5
Adriaan <adriaan@solis-studios.com>
2021-01-26 21:14:04 +0200

    Fixed data retrieval

--- Routes/user.js
+++ Routes/user.js
@@ -4,34 +4,24 @@ const User = require('../Models/User');
 
 router.get('/getdata', tokenVerification, async (req, res) => {
 
-    console.log(req.user);
-    const userId = req.user._id;
-    console.log(userId);
+  console.log(req.user);
+  const userId = req.user._id;
+  console.log(userId);
 
-    var companyName = "fok oll";
-    var userEmail = "niks";
-    var cellNum =  "kont oll";
-    var website = "warie";
+  var companyName = "fok oll";
+  var userEmail = "niks";
+  var cellNum = "kont oll";
+  var website = "warie";
 
-    User.findOne({ _id: userId }).then(user => {
-      if (user) {
-        
-        companyName = user.companyName
-        userEmail = user.userEmail
-        cellNum = user.cellNum
-        website = user.website
-      } else {
-        return res.status(400).json({ userId: "User not found" });  
-        }
-    });
-    //Logic
-    //Database callings to get valid data for userId
+  User.findOne({ _id: userId }).then(user => {
+    if (user) {
 
-    //Update this object
-    //||
-    //var totalclicks = db.clicks
-    
-    var response = {
+      companyName = user.companyName
+      userEmail = user.userEmail
+      cellNum = user.cellNum
+      website = user.website
+
+      var response = {
         totalClicks: 123,
         monthlyClicks: 20,
         name: companyName,
@@ -44,7 +34,12 @@ router.get('/getdata', tokenVerification, async (req, res) => {
         }
       }
 
-    res.send(response);
+      res.send(response);
+
+    } else {
+      return res.status(400).json({ userId: "User not found" });
+    }
+  });
 })
 
 module.exports = router;
